7ec0262b3f52a64684ededd2fef0e4a6238a1eaa
Ayaz <aayyaazz123@gmail.com>
2021-05-15 19:25:08 +0530

    List file update

---
+++ listandtuples.py
@@ -0,0 +1,27 @@
+# Print the list using []
+a = [18,26,33,64,598,69,76,82,93,3410,45,1234,76]
+
+#  Print the list using print() function
+print(a)
+
+# Access using index using a[0], a[1], a[2]
+print(a[0])
+print(a[1])
+print(a[2])
+# If try to find any index value which does not exist then it will show error
+
+# Update any index value
+a[0] = 54 #You can update any index value like this
+print(a)
+
+# We can create the list with items of differnt datatypes
+c = [54, "Ayaz", True, 43.3]
+print(c)
+
+# List Slicing
+friends = ["Harry", "Divya", "Rohan", "Ayaz", "Shraddha", "Akash", 45]
+print(friends[0:4])
+print(friends[-4:])
+
+# List Methods
+
--- twikle.py
+++ twikle.py
@@ -1,9 +1,10 @@
-'''
+a = '''
 Twinkle, twinkle, little star,
 How I wonder what you are!
 Up above the world so high,
 Like a diamond in the sky.
 '''
+print(a)
 
 print('''Twinkle, twinkle, little star,
 How I wonder what you are!
